1c45bec220fbc8c0777561b02de96bbd47e8bb61
milestarricone <milestarricone2@gmail.com>
2024-11-11 11:56:08 -0500

    2 sections of express lab done

--- server.js
+++ server.js
@@ -6,3 +6,34 @@ const PORT = 3000;
 
 app.use(morgan(`dev`));
 
+app.get(`/greetings/:username`, (req, res) =>  {
+    const username = req.params.username;
+    res.send(`Hello There, ${username}`)
+})
+
+app.get(`/roll/:number`, (req, res) => {
+    const diceNumber = req.params.number;
+    const randomNumber = Math.floor(Math.random() * diceNumber) + 1
+    res.send(`you rolled a ${randomNumber}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port 3000`)
+})
